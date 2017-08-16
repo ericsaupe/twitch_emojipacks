ddd729e712739827be3cc4a7bcd55d2105bd2286
Eric Saupe <ericsaupe@gmail.com>
2017-08-16 10:48:30 -0700

    Changed to convert Twitch to Emojipacks yaml

--- twitch_to_slack.rb
+++ twitch_to_slack.rb
@@ -1,39 +1,40 @@
 require 'httparty'
 require 'pry'
+require 'yaml'
 
 class TwitchToSlack
   include HTTParty
 
-  def initialize(twitch_channel, slack_group)
+  def initialize(twitch_channel)
     @twitch_channel = twitch_channel
-    @slack_group = slack_group
+  end
+
+  def save_twitch_emoticons_to_emojipacks_yaml
+    twitch_emotes = get_twitch_emoticons
+    yaml_content = to_emojipacks_yaml(twitch_emotes)
+    # Store to Emojipacks compatible YAML
+    File.open("emojipacks/#{@twitch_channel}.yaml", 'w') {|f| f.write yaml_content }
   end
 
   ##
   # Fetches twitch emoticons from channel_name
   #
   def get_twitch_emoticons
-    # Get Emoticon Set ID
-    # TODO: Figure out how to get this number from the API
-    emoticon_set = 17937 # MoonMoon_OW Emoticon Set
     # Get Emoticons for Channel
     headers = {
-      'Accept' => 'application/vnd.twitchtv.v5+json',
+      'Accept' => 'application/vnd.twitchtv.v3+json',
       'Client-ID' => 'efq3aoz1vvrtac1wz0ojszl54p6uuw'
     }
-
     http_response = self.class.get(
-      'https://api.twitch.tv/kraken/chat/emoticons',
+      "https://api.twitch.tv/kraken/chat/#{@twitch_channel}/emoticons",
       headers: headers
     )
     response = JSON.parse(http_response.body, symbolize_names: true)
     emoticons = response[:emoticons]
     emoticons = emoticons.select do |e|
-      e[:images].select do |i|
-        i[:emoticon_set] == emoticon_set
-      end.count > 0
+      e[:state] == 'active' && e[:subscriber_only]
     end
-    clean_selected_emoticons(emoticons)
+    emoticons
   end
 
   ##
@@ -46,37 +47,26 @@ class TwitchToSlack
   private
 
   ##
-  # Gets the Twitch user_id for the @twitch_channel
+  # Cleans the raw emoticon response from Twitch into a useful hash for Emojipacks
   #
-  def get_twitch_user_id
-    headers = {
-      'Accept' => 'application/vnd.twitchtv.v5+json',
-      'Client-ID' => 'efq3aoz1vvrtac1wz0ojszl54p6uuw'
+  def to_emojipacks_yaml(raw_emoticon_response)
+    result_hash = {
+      'title' => @twitch_channel,
+      'emojis' => []
     }
-    query = {
-      login: @twitch_channel
-    }
-
-    http_response = self.class.get(
-      'https://api.twitch.tv/kraken/users',
-      query: query,
-      headers: headers
-    )
-    response = JSON.parse(http_response.body, symbolize_names: true)
-    response[:users].first[:_id]
-  end
-
-  ##
-  # Cleans the raw emoticon response from Twitch into a useful hash
-  #
-  def clean_selected_emoticons(raw_emoticon_response)
-    cleaned_emoticons = {}
     raw_emoticon_response.each do |e|
-      cleaned_emoticons[e[:regex]] = e[:images].first[:url]
+      result_hash['emojis'] << { 'name' => e[:regex].downcase, 'src' => e[:url] }
     end
-    cleaned_emoticons
+    result_hash.to_yaml
   end
 end
 
-connector = TwitchToSlack.new('moonmoon_ow', 'triggeredbytime')
-twitch_emotes = connector.get_twitch_emoticons
+if ARGV[0].nil?
+  puts "Missing Twitch Channel argument"
+else
+
+  connector = TwitchToSlack.new(ARGV[0])
+  puts "Getting subscriber only emotes for #{ARGV[0]}"
+  connector.save_twitch_emoticons_to_emojipacks_yaml
+  puts "Emojipack saved to: emojipacks/#{ARGV[0]}.yaml"
+end
